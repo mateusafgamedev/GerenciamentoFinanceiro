8a99690eff5e5bcc7ab79097a71560b5e2a951ce
mateusafgamedev <mateusaf.gamedev@gmail.com>
2026-04-26 15:37:28 -0300

    Criação da classe AppDbContext para conexão e manipulação do DataBase e realização das migrações

---
+++ GerenciamentoFinanceiro/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace GerenciamentoFinanceiro.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+
+    }
+}
