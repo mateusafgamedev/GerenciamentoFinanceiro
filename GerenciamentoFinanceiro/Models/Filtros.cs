d3e08eb6ad7a9c9e317b67820c07cb0f276cf65a
mateusafgamedev <mateusaf.gamedev@gmail.com>
2026-04-27 04:47:50 -0300

    Inicando criação da lógica de filtragem da página

---
+++ GerenciamentoFinanceiro/Models/Filtros.cs
@@ -0,0 +1,17 @@
+﻿namespace GerenciamentoFinanceiro.Models
+{
+    public class Filtros
+    {
+
+        public Filtros(string filtroString)
+        {
+            FiltroString = filtroString ?? "todos-todos-todos";
+        }
+
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string TransacaoId { get; set; }
+        public string DataOperacao { get; set; }
+
+    }
+}
