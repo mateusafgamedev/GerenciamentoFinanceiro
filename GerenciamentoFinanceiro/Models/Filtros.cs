2eacf87e7df7248fe6fa29b5e365562aa0281d3e
Mateus Ferreira <mateusanjos213@gmail.com>
2026-04-28 14:44:58 -0300

    Criação da classe de filtros para filtragem dos registros

---
+++ GerenciamentoFinanceiro/Models/Filtros.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciamentoFinanceiro.Models
+{
+    public class Filtros
+    {
+
+        public Filtros(string filtrostring)
+        {
+            FiltroString = filtrostring ?? "todos-todos-todos";
+        }
+
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string TransacaoId { get; set; }
+        public string DataOperacao { get; set; }
+    }
+}
