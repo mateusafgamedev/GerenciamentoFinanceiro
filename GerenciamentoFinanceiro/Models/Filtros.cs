b0f63c5847713927e2073d478c3b86b9ed166486
Mateus Ferreira <mateusanjos213@gmail.com>
2026-04-28 15:21:26 -0300

    Finalização da classe de filtros

--- GerenciamentoFinanceiro/Models/Filtros.cs
+++ GerenciamentoFinanceiro/Models/Filtros.cs
@@ -6,11 +6,38 @@
         public Filtros(string filtrostring)
         {
             FiltroString = filtrostring ?? "todos-todos-todos";
+            string[] filtros = FiltroString.Split('-');
+
+            CategoriaId = filtros[0];
+            TransacaoId = filtros[1];
+            DataOperacao = filtros[2];
         }
 
         public string FiltroString { get; set; }
         public string CategoriaId { get; set; }
         public string TransacaoId { get; set; }
         public string DataOperacao { get; set; }
+
+
+        public bool TemCategoria => CategoriaId.ToLower() != "todos";
+        public bool TemTransacao => TransacaoId.ToLower() != "todos";
+        public bool TemDataOperacao => DataOperacao.ToLower() != "todos";
+
+
+        public static Dictionary<string, string> ObterDataOperacao()
+        {
+            return new Dictionary<string, string>
+            {
+                { "passado", "Operações Anteriores" },
+                { "futuro", "Operações Futuras" },
+                { "hoje", "Operações de Hoje" }
+            };
+        }
+
+
+        public bool EPassado => DataOperacao.ToLower() == "passado";
+        public bool EFuturo => DataOperacao.ToLower() == "futuro";
+        public bool EHoje => DataOperacao.ToLower() == "hoje";
+
     }
 }
