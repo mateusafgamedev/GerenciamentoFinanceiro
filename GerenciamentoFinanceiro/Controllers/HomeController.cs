25cb141dac6af349024275ad8ea24a534529aff2
Mateus Ferreira <mateusanjos213@gmail.com>
2026-04-29 19:06:49 -0300

    Criação da ActionIndex

--- GerenciamentoFinanceiro/Controllers/HomeController.cs
+++ GerenciamentoFinanceiro/Controllers/HomeController.cs
@@ -1,15 +1,62 @@
+using GerenciamentoFinanceiro.Data;
 using GerenciamentoFinanceiro.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GerenciamentoFinanceiro.Controllers
 {
     public class HomeController : Controller
-    {
-
-        public IActionResult Index()
+    { 
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public IActionResult Index(string id)
+        {
+
+            var filtros = new Filtros(id);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Transacoes = _context.Transacoes.ToList();
+
+            IQueryable<Financeiro> consulta = _context.Financas
+                .Include(t => t.Transacao)
+                .Include(c => c.Categoria);
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(f => f.CategoriaId == filtros.CategoriaId);
+            };
+
+            if (filtros.TemTransacao)
+            {
+                consulta = consulta.Where(f => f.TransacaoId == filtros.TransacaoId);
+            };
+
+            if (filtros.TemDataOperacao)
+            {
+               if (filtros.EPassado)
+                {
+                    consulta = consulta.Where(f => f.DataDaOperacao < DateTime.Today);
+                }
+                else if (filtros.EFuturo)
+                {
+                    consulta = consulta.Where(f => f.DataDaOperacao > DateTime.Today);
+                }
+                else if (filtros.EHoje)
+                {
+                    consulta = consulta.Where(f => f.DataDaOperacao == DateTime.Today);
+                }
+            }
+
+            var finacas = consulta.OrderBy(f => f.DataDaOperacao).ToList();
+
+
+            return View(finacas);
         }
     }
 }
