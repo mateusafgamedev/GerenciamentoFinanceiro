f2758c0532d1fbf6dd453864a70069f9fb6ffb86
mateusafgamedev <mateusaf.gamedev@gmail.com>
2026-08-17 21:03:27 -0300

    Criação do modelo de registro totais das finaças

---
+++ GerenciamentoFinanceiro/Models/RegistrosFinanceiros.cs
@@ -0,0 +1,15 @@
+﻿namespace GerenciamentoFinanceiro.Models
+{
+    public class RegistrosFinanceiros
+    {
+
+        public string CategoriaNome { get; set; }
+        public string TransacaoNome { get; set; }
+        public string DataOperacao { get; set; }
+        public string Lucros { get; set; }
+        public string Despesas { get; set; }
+        public string ValorCategoria { get; set; }
+        public string Diferenca { get; set; }
+        
+    }
+}
